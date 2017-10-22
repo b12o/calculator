5f1d4ab859245993f71f0ffabd8048665ab7a48a
Barnabas Edubio <barny.edubio@gmail.com>
2017-10-23 01:37:07 +0200

    live viewing of calculation on screen now possible

--- script.js
+++ script.js
@@ -8,44 +8,59 @@ var calculate = "",
 
 function calculateInput() {
     $(".button").on("click", function () {
+
         if (this.id.match(/\d/)) { // a number
             currentEntry += this.id.match(/\d/)[0];
+            $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
             console.log("calculation: " + calculate + ", current entry: " + currentEntry);
         }
         else {
             // clear entire input
-            if (calculate.length !== 0 && this.id === "button_ac") {
+            if ((calculate.length !== 0 || currentEntry !== 0) && this.id === "button_ac") {
                 calculate = "";
                 currentEntry = "";
+                $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                 console.log("calculation: " + calculate + ", current entry: " + currentEntry);
             }
+
+            else if (currentEntry.length === 0 && this.id === "button_dot") {
+                currentEntry = "0.";
+                $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
+            }
+
             else if (currentEntry.length !== 0) {
                 switch (this.id) {
                     case "button_ce":
                         currentEntry = "";
+                        $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                         break;
                     case "button_plus":
                         calculate += currentEntry;
                         calculate += " + ";
                         currentEntry = "";
+                        $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                         break;
                     case "button_minus":
                         calculate += currentEntry;
                         calculate += " - ";
                         currentEntry = "";
+                        $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                         break;
                     case "button_mult":
                         calculate += currentEntry;
-                        calculate += " * ";
+                        calculate += " x ";
                         currentEntry = "";
+                        $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                         break;
                     case "button_div":
                         calculate += currentEntry;
                         calculate += " / ";
                         currentEntry = "";
+                        $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                         break;
                     case  "button_dot":
                         if (!isDecimal(currentEntry)) currentEntry += ".";
+                        $(".all-entries").html("<h2>" + calculate + currentEntry + "</h2>");
                         break;
                     case "button_enter":
                     case "button_hide_enter":
@@ -58,14 +73,11 @@ function calculateInput() {
                         break;
                 }
                 console.log("calculation: " + calculate + ", current entry: " + currentEntry);
-            } else {
-
             }
         }
     });
 }
 
 function isDecimal(val) {
-    return val.match(/\./)[0].length === 1;
+    return (val.match(/\./) !== null && val.match(/\./)[0].length === 1);
 }
-
